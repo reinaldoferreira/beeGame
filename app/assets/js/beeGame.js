d6630ec4dd13350b43a9e75abc214dec9658c51f
Reinaldo Ferreira <reinaldocesarferreira@gmail.com>
2016-07-01 16:10:38 +0100

    function to play the game

--- app/assets/js/beeGame.js
+++ app/assets/js/beeGame.js
@@ -29,3 +29,25 @@ function whichBeeToHit(list) {
   var position = randomNumber(list.length)
   return position
 }
+
+var queenBee = new Bee('Queen Bee', 'queen', 100);
+function attackRandomBee() {
+
+  switch (randomNumber(3)) {
+    case 1:
+      console.log('Queen')
+      if (queenBee.healthPoints > 0) {
+        queenBee.decreaseHP(8)
+        console.log(queenBee.healthPoints);
+        if (queenBee.healthPoints < 0) console.log('The Queen Bee died, Play again?');
+      } else {
+        console.log('The Queen Bee died, Play again?');
+      }
+      break;
+    case 2:
+      console.log('Worker')
+      break;
+    default:
+      console.log('Drone')
+  }
+}
