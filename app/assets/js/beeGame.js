78024c6ba6ab60c8f10439e56f25a7a52a4492d0
Reinaldo Ferreira <reinaldocesarferreira@gmail.com>
2016-07-02 13:39:19 +0100

    attacking all the bees

--- app/assets/js/beeGame.js
+++ app/assets/js/beeGame.js
@@ -6,18 +6,18 @@ function Bee(name, kind, healthPoints) {
 
 Bee.prototype.decreaseHP = function (pointsToDecrease) {
   return this.healthPoints -= pointsToDecrease;
-};
+}
 
 
 function fabricOfBees(numberOfBees, name, kind, healthPoints) {
   var list = [];
 
   for (var i = 0; i < numberOfBees; i++) {
-    var beeNameHelper = i + 1;
+    var beeNameHelper = i + 1
     list[i] = new Bee(name + " " + beeNameHelper, kind, healthPoints)
   }
 
-  return list;
+  return list
 }
 
 function randomNumber(number) {
@@ -26,28 +26,33 @@ function randomNumber(number) {
 }
 
 function whichBeeToHit(list) {
-  var position = randomNumber(list.length)
+  var position = randomNumber(list.length) - 1
   return position
 }
 
-var queenBee = new Bee('Queen Bee', 'queen', 100);
+var queenBee = new Bee('Queen Bee', 'queen', 100)
+var listOfWorkers = fabricOfBees(8, "Worker Bee", "worker", 75)
+var listOfDrones = fabricOfBees(12, "Drone Bee", "drone", 50)
+
 function attackRandomBee() {
 
   switch (randomNumber(3)) {
     case 1:
-      console.log('Queen')
+      console.log('- - - Queen - - -')
       if (queenBee.healthPoints > 0) {
         queenBee.decreaseHP(8)
-        console.log(queenBee.healthPoints);
-        if (queenBee.healthPoints < 0) console.log('The Queen Bee died, Play again?');
+        console.log(queenBee.healthPoints)
+        if (queenBee.healthPoints < 0) console.log('The Queen Bee died, Play again?')
       } else {
-        console.log('The Queen Bee died, Play again?');
+        console.log('The Queen Bee died, Play again?')
       }
       break;
     case 2:
-      console.log('Worker')
+      console.log('- - - Worker - - -')
+      listOfWorkers[whichBeeToHit(listOfWorkers)].decreaseHP(10)
       break;
     default:
-      console.log('Drone')
+      console.log('- - - Drone - - -')
+      listOfDrones[whichBeeToHit(listOfDrones)].decreaseHP(12)
   }
 }
