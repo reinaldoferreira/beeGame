4dfcc60a0ee96b76bb31079f6b4d27bb48cd1305
Reinaldo Ferreira <rferreira@macmillan.org.uk>
2016-07-29 11:54:52 +0100

    game restarting when the queen dies

--- app/assets/_js/index.js
+++ app/assets/_js/index.js
@@ -8,40 +8,45 @@ function randomNumber(number) {
   return random;
 }
 
+function attackProcess(beePosition, damage) {
+  var bee = document.getElementById(beePosition);
+  bee.className += ' is-bee--attacked';
+
+  setTimeout(function() {
+    if (allTheBees[beePosition].healthPoints >= 0) {
+      allTheBees[beePosition].decreaseHP(damage);
+
+      if (allTheBees[beePosition].healthPoints <= 0) {
+        if (allTheBees[beePosition].kind === 'queen') {
+          alert('The Queen Bee died! You lose and the will be restarted');
+          queenBee = new Bee('Queen Bee', 'queen', 100);
+          listOfWorkers = fabricOfBees(5, 'Worker Bee', 'worker', 75);
+          listOfDrones = fabricOfBees(8, 'Drone Bee', 'drone', 50);
+          allTheBees = getArrayOfBees(queenBee, listOfWorkers, listOfDrones);
+        } else {
+          allTheBees.splice(beePosition, 1);
+        }
+      }
+      printBees(allTheBees);
+      attackingFlag = false;
+    }
+  }, 200);
+}
+
 function attackRandomBee() {
   var beePosition = randomNumber(allTheBees.length);
 
-  function attackProcess(damage) {
-    var bee = document.getElementById(beePosition);
-    bee.className += ' is-bee--attacked';
-
-    setTimeout(function() {
-      if (allTheBees[beePosition].healthPoints >= 0) {
-        allTheBees[beePosition].decreaseHP(damage);
-
-        if (allTheBees[beePosition].healthPoints <= 0) {
-          if (allTheBees[beePosition].kind === 'queen') {
-            alert('The Queen Bee died! Play again?');
-          }
-          allTheBees.splice(beePosition, 1);
-        }
-        printBees(allTheBees);
-        attackingFlag = false;
-      }
-    }, 200);
-  }
-
   switch (allTheBees[beePosition].kind) {
     case 'drone':
-      attackProcess(12);
+      attackProcess(beePosition, 12);
       break;
 
     case 'worker':
-      attackProcess(10);
+      attackProcess(beePosition, 10);
       break;
 
     default:
-      attackProcess(8);
+      attackProcess(beePosition, 8);
   }
 }
 
